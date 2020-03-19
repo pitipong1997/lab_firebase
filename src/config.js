accc2fb27d37c88aaab9bfbb12879987f41dbdf6
pitipong1997 <5835512033@psu.ac.th>
2020-03-19 19:57:30 +0700

    first commit

---
+++ src/config.js
@@ -0,0 +1,13 @@
+const config = {
+
+    apiKey: "AIzaSyDXsKl_coq8LUYeSfUQAPqqMHu5OtvZcmw",
+    authDomain: "todo2-c4073.firebaseapp.com",
+    databaseURL: "https://todo2-c4073.firebaseio.com",
+    projectId: "todo2-c4073",
+    storageBucket: "todo2-c4073.appspot.com",
+    messagingSenderId: "1025373407191",
+    appId: "1:1025373407191:web:3331d467d7660d03feed92",
+    measurementId: "G-C6Q9BJBXCL"
+    };
+  
+    export default config;
